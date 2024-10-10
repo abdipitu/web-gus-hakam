37cb34f8f4fb4807a9ae89929442c507c8d7eb86
abdipitu <humambdlh@gmail.com>
2024-10-10 19:58:43 +0700

    add all file

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#D6EFD8',
+      },
+      fontFamily: {
+        'karla': ['Karla', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
